95c1ed06ab3d2e5807e79b55be3a4d6e4d4ca740
Glenn <fischer@apiomat.com>
2016-12-12 13:29:14 +0100

    [AOM] Change MetaModel

---
+++ src/com/apiomat/nativemodule/gitmod/GitClass.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2011 - 2016, Apinauten GmbH
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.apiomat.nativemodule.gitmod;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+import com.apiomat.nativemodule.*;
+
+
+import com.apiomat.nativemodule.basics.*;
+import com.apiomat.nativemodule.AuthState;
+/**
+* Generated class for your GitClass data model
+*
+* DO NOT CHANGE ANY CODE EXCEPT CLASS ANNOTATIONS OR CLASS ATTRIBUTES HERE!
+* EVERYTHING ELSE WILL GET OVERWRITTEN!
+*
+*/
+@SuppressWarnings( "unused" )
+@Model( moduleName = "GitMod",
+    hooksClassNameTransient = "com.apiomat.nativemodule.gitmod.GitClassHooksTransient", 
+    hooksClassNameNonTransient = "com.apiomat.nativemodule.gitmod.GitClassHooksNonTransient", 
+            isTransient = false,     requiredUserRoleCreate=UserRole.User, requiredUserRoleRead=UserRole.User,
+    requiredUserRoleWrite=UserRole.Owner, restrictResourceAccess=false,
+    allowedRolesCreate={}, allowedRolesRead={},
+    allowedRolesWrite={}, allowedRolesGrant={})
+public class GitClass extends AbstractClientDataModel implements IModel<GitClass>
+{
+    /**
+     * Contains the name of the module that this model belongs to
+     */
+    public static final String MODULE_NAME = "GitMod";
+    /**
+     * Contains the name of the model
+     */
+    public static final String MODEL_NAME = "GitClass";
+
+    /** class specific attributes */
+    @Mandatory
+    private String gitAttr = null;
+    /**
+     * Protected constructor; to create a new instance, use the createObject() method
+     */
+    public GitClass ()
+    {}
+
+    /**
+     * Returns the name of the module where this class belongs to
+     */
+    @Override
+    public String getModuleName( )
+    {
+        return MODULE_NAME;
+    }
+
+    /**
+     * Returns the name of the model
+     */
+    @Override
+    public String getModelName( )
+    {
+        return MODEL_NAME;
+    }
+
+    public String getGitAttr()
+    {
+         return this.gitAttr;
+    }
+
+    public void setGitAttr( String arg )
+    {
+        this.gitAttr = arg;
+    }
+
+}
